f1e2bbb3d50ed12db1bd965003250b7c2453259e
19980410 <kaoru.kobayashi.infratop@gmail.com>
2020-06-12 10:58:29 +0900

    "新着通知がある時にわかるように修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.where.not(visitor_id: current_user.id)
+    #あとで処理をupdate_allにかえる
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    def notification_check?
+        notification = current_user.passive_notifications.where(checked: false)
+        notification.exists? ? true : false 
+    end
 end
