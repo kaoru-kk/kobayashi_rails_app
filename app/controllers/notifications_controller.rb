28df470d046cc0b17417ff5c0cb6f82a2c451371
19980410 <kaoru.kobayashi.infratop@gmail.com>
2020-06-12 11:44:19 +0900

    通知fix

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,6 @@
 class NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.where.not(visitor_id: current_user.id)
-    #あとで処理をupdate_allにかえる
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
-    end
+    @notifications.where(checked: false).update_all(checked: true)
   end
 end
