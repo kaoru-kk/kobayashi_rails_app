551e3970962d52aee48c5d710fe010958c85cedf
19980410 <kaoru.kobayashi.infratop@gmail.com>
2020-06-16 13:21:37 +0900

    notificationのhelper移行

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-    def notification_check?
-        notification = current_user.passive_notifications.where(checked: false)
-        notification.exists? ? true : false 
-    end
 end
--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,7 @@
 module NotificationsHelper
+    module_function
+    def notification_check?(current_user)
+        notification = current_user.passive_notifications.where(checked: false)
+        notification.exists? ? true : false 
+    end
 end
